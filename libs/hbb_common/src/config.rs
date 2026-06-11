914e4c55f5d06e10f24ce8a79669cb761967603b
OnSpace <comunitariogpt@gmail.com>
2026-06-11 11:57:53 -0300

    fix: vec! for HashMap collect (edition 2018)

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -80,7 +80,7 @@ lazy_static::lazy_static! {
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-    pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = RwLock::new([("hide-powered-by-me".to_owned(),"Y".to_owned())].into_iter().collect());
+    pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = RwLock::new(vec![("hide-powered-by-me".to_owned(),"Y".to_owned())].into_iter().collect());
 }
 
 #[cfg(target_os = "android")]
